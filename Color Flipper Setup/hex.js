b640a19cf1b058758728639a213ffc95e45c4f26
djvic1476 <90651115+djvic1476@users.noreply.github.com>
2022-02-01 14:56:56 -0700

    we added things to one of the hex files

--- Color Flipper Setup/hex.js
+++ Color Flipper Setup/hex.js
@@ -1,1 +1,16 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+btn.addEventListener("click", function() {
+    let hexcolor = "#";
+    for (let i =0; i < 6; i++){
+        hexcolor += hex[getRandomNumber];
+    }
+    consle.log(hexcolor);
+});
+    color.textContent = hexcolor;
+    document.body.style.backgroundColor = hexcolor;
+function getRandomNumber () {
+    return math.floor(math.random() * hex.length);
+}
